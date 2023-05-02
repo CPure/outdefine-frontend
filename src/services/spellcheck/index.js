e81cb72b689f5bdeffd008de7c5b13e8adf26a1d
Cpure <carlospures@gmail.com>
2023-05-02 10:54:32 -0500

    structure  and service

---
+++ src/services/spellcheck/index.js
@@ -0,0 +1,12 @@
+import { api } from '../axios'
+
+
+export const getSpellcheck = async (word) => {
+  const response = await api.get(`/api/spellcheck/${word}`)
+
+  return response.data
+}
+
+export default {
+    getSpellcheck,
+}
